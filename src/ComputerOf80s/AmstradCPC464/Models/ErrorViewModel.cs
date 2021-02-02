42ab039d78a3507cf2fa1aa8ff66725ce9e624cd
Nicolas CLERC <nicolas.clerc@sablefin.net>
2021-02-02 23:14:25 +0100

    addind demo projects

---
+++ src/ComputerOf80s/AmstradCPC464/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AmstradCPC464.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

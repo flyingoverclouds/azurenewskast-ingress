ab638e0bd2160a564e7f95d9daab5929acb6abde
Nicolas CLERC <nicolas.clerc@sablefin.net>
2021-02-03 18:39:01 +0100

    ingress avec AksHttpRouting

--- deployment/01-noingress/00-deployNoIngress.cmd
+++ deployment/01-noingress/00-deployNoIngress.cmd
@@ -3,6 +3,7 @@ SET KNS=computerof80s-noingress
 
 call kubectl create namespace %KNS%
 
-call kubectl apply -f deployments.yaml --namespace %KNS%
+REM call kubectl apply -f deployments.yaml --namespace %KNS%
+call kubectl apply -f replicas.yaml --namespace %KNS%
 
 call kubectl apply -f services.yaml --namespace %KNS%
